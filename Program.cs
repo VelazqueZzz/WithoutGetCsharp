eb12d2a188083c520b4548b5c36adcff843d8fde
patro <patro@DESKTOP-OJDTVT2>
2023-09-10 20:11:35 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class MyClass
+{
+    private string code;
+    public MyClass(uint n)
+    {
+        number = n;
+
+    }
+    public override string ToString()
+    {
+        return code;
+
+    }
+    public uint number
+    {
+        set
+        {
+            uint num = value;
+            code = "";
+            do
+            {
+                code = (num % 2) + code;
+                num >>= 1;
+
+            }while(num != 0);
+        }
+    }
+
+}
+class WithoutGetDemo
+{
+    static void Main()
+    {
+        MyClass obj = new MyClass(5);
+        Console.WriteLine("Binarnii code chisla 5:\t" + obj);
+        obj.number = 45;
+        Console.WriteLine("Binarnii code chisla 45:\t" + obj);
+    }
+}
